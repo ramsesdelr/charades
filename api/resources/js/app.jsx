3efcd976d749357be38dd26e6af8be2e97b0d290
Ramsés Del Rosario <ramsesdelr@gmail.com>
2019-04-05 21:18:09 -0400

    created react-app

---
+++ api/resources/js/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+	<h1>
+	Tamo aqui
+	</h1>
+	,
+	document.getElementById('app')
+);
---
+++ api/resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div id="app">
+	</div>
+{{-- <script src="/js/manifest.js"></script>
+<script src="/js/vendor.js"></script> --}}
+<script src="/js/app.js"></script>
+</body>
+</html>
