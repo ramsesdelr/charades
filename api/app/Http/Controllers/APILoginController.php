c71548d583bbb8a924fbaa7a8ac8971979a5b43c
Ramsés Del Rosario <ramsesdelr@gmail.com>
2020-08-20 01:13:10 -0400

    removed comma from syntax error

--- api/app/Http/Controllers/APILoginController.php
+++ api/app/Http/Controllers/APILoginController.php
@@ -42,7 +42,7 @@ class APILoginController extends Controller
         $user_data = Auth::user($token);
         
         if($request->get('match_id') != '') {
-            $newPlayer = $usersMatchRepo->addUserToMatch($user_data->id, base64_decode($request->get('match_id')), );
+            $newPlayer = $usersMatchRepo->addUserToMatch($user_data->id, base64_decode($request->get('match_id')));
         }
         
         return response()->json(compact('token', 'user_data'));
